99d6022b649e3a375dc271978fdc97c97b9cf62d
Alexey Solod <charmy.machine@yandex.ru>
2025-09-17 13:08:37 +0300

    Update schema for movie

--- app/schemas/movie.py
+++ app/schemas/movie.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class MovieBase(BaseModel):
     movie_id: int
-    movie_title: str
-    description: str
-    rating_mpaa: str
+    movie_title: str = Field(..., min_length=3, max_length=100)
+    description: str = Field(..., min_length=3, max_length=255)
+    rating_mpaa: str = Field(..., min_length=1, max_length=5)
 
 
 class Movie(MovieBase):
