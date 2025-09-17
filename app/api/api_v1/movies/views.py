d6cb2fff2b61fbb5ac3845ea36a97674dd0f0f11
Alexey Solod <charmy.machine@yandex.ru>
2025-09-17 13:09:20 +0300

    Create endpoint for creating movie in catalog

--- app/api/api_v1/movies/views.py
+++ app/api/api_v1/movies/views.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, status, Form
 
 from .crud import MOVIES_LIST
 from .dependencies import prefetch_movie
 from schemas.movie import Movie
+from random import randint
 
 router = APIRouter(
     prefix="/movies",
@@ -21,4 +22,17 @@ def read_movies_list():
 def read_movie_details(
     movie: Annotated[Movie, Depends(prefetch_movie)],
 ) -> Movie | None:
+    return movie
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    movie_data: Annotated[Movie, Form()],
+):
+    movie = movie_data.model_dump()
+    movie.update({"movie_id": randint(1, 1000)})
     return movie
