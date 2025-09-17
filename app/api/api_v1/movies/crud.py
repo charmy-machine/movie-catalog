597d5a9d5d2d35a0e939b36b9f38168c47c49227
Alexey Solod <charmy.machine@yandex.ru>
2025-09-17 19:43:06 +0300

    Create method for work with json file with movies

--- app/api/api_v1/movies/crud.py
+++ app/api/api_v1/movies/crud.py
@@ -1,10 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
+from core.config import MOVIES_STORAGE_FILEPATH
 from schemas.movie import Movie, MovieCreate, MovieUpdate, MoviePartialUpdate
 
 
 class MovieStorage(BaseModel):
     slug_to_movie: dict[str, Movie] = {}
+
+    def save_state(self) -> None:
+        MOVIES_STORAGE_FILEPATH.write_text(self.model_dump_json(indent=2))
+
+    @classmethod
+    def from_state(cls) -> "MovieStorage":
+        if not MOVIES_STORAGE_FILEPATH.exists():
+            return MovieStorage()
+        return cls.model_validate_json(MOVIES_STORAGE_FILEPATH.read_text())
 
     def get(self) -> list[Movie]:
         return list(self.slug_to_movie.values())
@@ -15,10 +25,12 @@ class MovieStorage(BaseModel):
     def create(self, movie_in: MovieCreate) -> Movie:
         movie = Movie(**movie_in.model_dump())
         self.slug_to_movie[movie.slug] = movie
+        self.save_state()
         return movie
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_movie.pop(slug, None)
+        self.save_state()
 
     def delete(self, movie: Movie) -> None:
         self.delete_by_slug(slug=movie.slug)
@@ -26,30 +38,18 @@ class MovieStorage(BaseModel):
     def update(self, movie: Movie, movie_in: MovieUpdate) -> Movie:
         for field_name, value in movie_in:
             setattr(movie, field_name, value)
+        self.save_state()
         return movie
 
     def update_partial(self, movie: Movie, movie_in: MoviePartialUpdate):
         for field_name, value in movie_in.model_dump(exclude_unset=True).items():
             setattr(movie, field_name, value)
+        self.save_state()
         return movie
 
 
-storage = MovieStorage()
-
-storage.create(
-    MovieCreate(
-        movie_title="The Gentlemen",
-        description="Gangsters of all stripes share a drug farm. Guy Ritchie's swirling action comedy starring Matthew McConaughey and Hugh Grant",
-        rating_mpaa="R",
-        slug="1abc",
-    )
-)
-
-storage.create(
-    MovieCreate(
-        movie_title="Interstellar",
-        description="Humanity's next step will be the greatest",
-        rating_mpaa="PG-13",
-        slug="2abc",
-    ),
-)
+try:
+    storage = MovieStorage.from_state()
+except ValidationError:
+    storage = MovieStorage()
+    storage.save_state()
