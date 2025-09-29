11853c1b3d3fc480953ee9fd1f6e15e6c771c5f8
Alexey Solod <charmy.machine@yandex.ru>
2025-09-29 21:38:02 +0300

    Remove temporary API_TOKENS and USERS_DB

--- app/core/config.py
+++ app/core/config.py
@@ -9,26 +9,10 @@ LOGGER_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 )
 
-# Never store real tokens here!
-# Only fake values
-API_TOKENS: frozenset[str] = frozenset(
-    {
-        "dhhI4evsuyyM75G8QaYXww",
-        "ZjFMZQxI6BD4rKgiDd1Oiw",
-    }
-)
-
-# Only for demo
-# Never store real users in code
-USERS_DB: dict[str, str] = {
-    # username: password
-    "sam": "password",
-    "bob": "qwerty",
-}
-
 REDIS_HOST: str = "localhost"
 REDIS_PORT: int = 6379
 REDIS_DB: int = 0
 REDIS_DB_TOKENS: int = 2
+REDIS_DB_USERS: int = 4
 
 REDIS_TOKENS_SET_NAME = "tokens"
