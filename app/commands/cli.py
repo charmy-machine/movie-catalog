eddc5f994c34923f52d46a15c7241d37bf21f582
Alexey Solod <charmy.machine@yandex.ru>
2025-10-06 21:29:05 +0300

    Remove CLI command hello

--- app/commands/cli.py
+++ app/commands/cli.py
@@ -1,7 +1,6 @@
 __all__ = ("app",)
 
 import typer
-from .hello import app as hello_app
 from .tokens import app as tokens_app
 
 app = typer.Typer(
@@ -17,5 +16,4 @@ def callback():
     """
 
 
-app.add_typer(hello_app)
 app.add_typer(tokens_app)
