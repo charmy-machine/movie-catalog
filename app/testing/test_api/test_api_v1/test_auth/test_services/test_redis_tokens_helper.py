d01dadf105063759884fe7da030313501c8ec2e7
Alexey Solod <charmy.machine@yandex.ru>
2025-10-15 20:43:51 +0300

    Create test for redis_tokens_helper

---
+++ app/testing/test_api/test_api_v1/test_auth/test_services/test_redis_tokens_helper.py
@@ -0,0 +1,14 @@
+from os import getenv
+from unittest import TestCase
+
+from api.api_v1.auth.services import redis_tokens
+
+if getenv("TESTING") != "1":
+    error_msg = "Environment is not ready for testing."
+    raise OSError(error_msg)
+
+
+class RedisTokensHelperTestCase(TestCase):
+    def test_generate_and_save_token(self) -> None:
+        new_token = redis_tokens.generate_and_save_token()
+        self.assertTrue(redis_tokens.token_exists(new_token))
