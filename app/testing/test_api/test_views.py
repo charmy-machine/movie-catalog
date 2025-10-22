923b5a565dae322754c2dc4b7e699416150bc43a
Alexey Solod <charmy.machine@yandex.ru>
2025-10-22 21:45:43 +0300

    Add parametrizing for API test

--- app/testing/test_api/test_views.py
+++ app/testing/test_api/test_views.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
@@ -7,13 +8,31 @@ client = TestClient(app)
 
 
 def test_root_view() -> None:
-    # TODO: fake data
-    name = "Alex"
+    response = client.get("/")
+    response_data = response.json()
+
+    assert response.status_code == status.HTTP_200_OK, response.text
+
+    expected_message = "Hello, Anonymous!"
+    assert response_data["message"] == expected_message, response_data
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        # TODO: fake data
+        "John",
+        "",
+        "John Smith",
+        "!@#$%",
+    ],
+)
+def test_root_view_custom_name(name: str) -> None:
     params = {"name": name}
     response = client.get("/", params=params)
     response_data = response.json()
 
     assert response.status_code == status.HTTP_200_OK, response.text
 
-    expected_message = f"Hello {name}!"
+    expected_message = f"Hello, {name}!"
     assert response_data["message"] == expected_message, response_data
